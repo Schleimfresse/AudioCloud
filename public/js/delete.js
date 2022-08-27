7a23869a294332d13d628ca65e1c5100415b90b6
Schleimfresse <87274892+Schleimfresse@users.noreply.github.com>
2022-08-27 18:56:37 +0200

    INITIAL COMMIT

---
+++ public/js/delete.js
@@ -0,0 +1,26 @@
+const main = document.querySelector("main");
+const secureDOM = document.getElementById('secure');
+async function load() {
+	const res = await fetch("/api");
+	const data = await res.json();
+	data.forEach((e) => {
+		const div = document.createElement("div");
+        div.setAttribute('class', 'box');
+        div.setAttribute('onclick', 'secure(this.children[0].innerText);');
+        div.textContent = 'Media: '
+		div.append(document.createElement("span"));
+        div.children[0].textContent = e.name;
+        main.append(div);
+	});
+}
+load();
+function secure(name) {
+secureDOM.style.display = 'flex';
+secureDOM.children[0].children[0].textContent = name}
+
+async function deleteobj(id) {
+    secureDOM.style.display = 'none';
+    await fetch(`/Media/${encodeURIComponent(id)}`, {
+        method: "DELETE"
+    });
+}
--- server.js
+++ server.js
@@ -1,18 +1,48 @@
 const express = require("express");
+const Datastore = require("nedb");
 const upload = require("express-fileupload");
 const app = express();
+const fs = require("fs");
 const port = process.env.PORT || 3000;
 const server = require("http").createServer(app);
 app.use(require("body-parser").json({ limit: "1mb" }));
 app.use(upload());
 app.use(express.static(__dirname + "/public"));
+server.listen(port, () => {
+	console.log(`app listening at Port: ${port}`);
+});
+const database = new Datastore("database.db");
+database.loadDatabase();
 
 app.get("/", (req, res) => {
-	res.sendFile(__dirname + "/public/index.html");
+	res.status(200);
 });
 
 app.get("/upload", (req, res) => {
+	res.status(200);
 	res.sendFile(__dirname + "/public/upload.html");
+});
+
+app.get("/delete", (req, res) => {
+	res.status(200);
+	res.sendFile(__dirname + "/public/delete.html");
+});
+
+app.delete("/Media/:id", (req, res) => {
+	try {
+		database.remove({ name: req.params.id });
+		fs.unlink(`./public/Media/${req.params.id}`, () => {
+			res.status(204);
+			res.sendFile(__dirname + "/public/success.html");
+		});
+	} catch (err) {
+		res.status(500);
+	}
+});
+
+app.get("/Media/:id", (req, res) => {
+	res.status(200);
+	res.sendFile(__dirname + "/Media/" + req.params.id);
 });
 
 app.post("/uploadstatus", (req, res) => {
@@ -20,11 +50,12 @@ app.post("/uploadstatus", (req, res) => {
 		let file = req.files.file;
 		let filename = file.name;
 
-		file.mv("./Media/" + filename, function (err) {
+		file.mv("./public/Media/" + filename, function (err) {
 			if (err) {
 				res.status(500);
 				res.send(err);
 			} else {
+				database.insert({ name: filename });
 				res.status(200);
 				res.sendFile(__dirname + "/public/success.html");
 			}
@@ -32,6 +63,19 @@ app.post("/uploadstatus", (req, res) => {
 	}
 });
 
-server.listen(port, () => {
-	console.log(`app listening at Port: ${port}`);
+app.get("/api", (request, response) => {
+	database.find({}, (err, data) => {
+		if (err) {
+			console.log("/api:", err);
+			response.end();
+			return;
+		}
+		response.json(data);
+	});
+});
+
+app.use(function (req, res) {
+	res.status(404);
+	res.sendFile(__dirname + "/public/404.html");
+	return;
 });
